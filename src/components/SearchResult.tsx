0ae64b2bcdf45fc6a4a6d910631a59176c7bbcd2
BisiDavi <oludavidconnect@gmail.com>
2022-12-19 17:21:28 +0100

    search result add

---
+++ src/components/SearchResult.tsx
@@ -0,0 +1,8 @@
+export default function SearchResult() {
+  return (
+    <div className="rounded-xl py-20 pl-10 pr-4 mt-20">
+      <h4 className="text-xl font-bold">Here&#39;s who we found</h4>
+    </div>
+  );
+}
+ 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import Layout from "@/layout";
 import Hero from "@/components/Hero";
 import Filters from "@/components/Filters";
+import SearchResult from "@/components/SearchResult";
 
 export default function Homepage() {
   return (
     <Layout>
       <Hero />
       <main className="content relative z-0 -mt-1">
-        <section className="container  z-10 mx-auto -mt-20">
+        <section className="container z-10 mx-auto -mt-20">
           <Filters />
+          <SearchResult />
         </section>
       </main>
       <style jsx>
