a54e44320ef03d071f7bc5597b519ecacf793532
BisiDavi <oludavidconnect@gmail.com>
2022-12-19 17:10:17 +0100

    styles updated

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -3,7 +3,7 @@ import Tag from "@/components/Tag";
 export default function Filters() {
   return (
     <>
-      <div className="filters w-80 rounded-tl-xl p-10">
+      <div className="filters w-80 rounded-tl-3xl p-10">
         <h4 className="text-2xl font-bold">Filters</h4>
         <p className="text-xl font-bold">Industries</p>
         <div className="tag-group mt-4 grid grid-cols-2 gap-4">
--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -6,7 +6,7 @@ interface Props {
 export default function Tag({ text = "This is a tag", bgColor }: Props) {
   return (
     <>
-      <div className="tag flex h-8 items-center rounded-md px-4 text-sm text-white">
+      <div className="tag flex h-8 items-center rounded-md px-4 text-sm text-white hover:opacity-80">
         {text}
       </div>
       <style jsx>
